1cc280f8235b841659b76eac013bb2193a85bad4
Santiago Gerhart <gerhartsantiago@gmail.com>
2024-12-23 00:30:34 -0300

    chore: update server code

--- Dookie.Server/Program.cs
+++ Dookie.Server/Program.cs
@@ -1,43 +1,12 @@
-﻿using LiteNetLib;
-using LiteNetLib.Utils;
+﻿using Dookie.Core.Network;
 
-var listener = new EventBasedNetListener();
-var server = new NetManager(listener);
-server.Start(3074);
-
-listener.ConnectionRequestEvent += OnListenerOnConnectionRequestEvent;
-listener.PeerConnectedEvent += OnListenerOnPeerConnectedEvent;
-listener.NetworkLatencyUpdateEvent += ListenerOnNetworkLatencyUpdateEvent;
+var server = new ServerNetworkManager();
+server.Start();
 
 while (!Console.KeyAvailable)
 {
-    server.PollEvents();
+    server.Update();
     Thread.Sleep(15);
 }
 
 server.Stop();
-return;
-
-void OnListenerOnConnectionRequestEvent(ConnectionRequest request)
-{
-    if (server.ConnectedPeersCount < 10 /* max connections */)
-        request.AcceptIfKey("SomeConnectionKey");
-    else
-        request.Reject();
-}
-
-void OnListenerOnPeerConnectedEvent(NetPeer peer)
-{
-    Console.WriteLine("We got connection: {0}", peer);
-    var writer = new NetDataWriter();
-    writer.Put("Hello client!");
-
-    peer.Send(writer, DeliveryMethod.ReliableOrdered);
-}
-
-void ListenerOnNetworkLatencyUpdateEvent(NetPeer peer, int latency)
-{
-    var writer = new NetDataWriter();
-    writer.Put($"Latency: {latency}");
-    peer.Send(writer, DeliveryMethod.ReliableOrdered);
-}
